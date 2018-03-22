daf0e35358b15ba28b26e0e5f6cc4df279e846fc
DTO-X1\Dto <chartchai.d@cmu.ac.th>
2018-03-23 06:59:03 +0700

    add the compose file

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -7,6 +7,17 @@ import org.springframework.stereotype.Service;
 public class GradeServiceImpl implements GradeService {
     @Override
     public Grade getGrade(double score) {
-        return Grade.builder().grade("A").build();
+        if (score > 80) {
+            return Grade.builder().grade("A").build();
+        }
+        else if (score > 75){
+            return Grade.builder().grade("B").build();
+        }else if (score > 60){
+            return Grade.builder().grade("C").build();
+        }else if (score > 33) {
+            return Grade.builder().grade("D").build();
+        } else
+            return Grade.builder().grade("F").build();
+
     }
 }
